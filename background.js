17a9efd3cec7d44ed85a0b924fcb087fc50b9493
Matthew Casperson <matthewcasperson@gmail.com>
2024-03-20 08:50:35 +1000

    Added comments

--- background.js
+++ background.js
@@ -1,26 +1,48 @@
 import * as messageTools from '/modules/messageTools.mjs';
 
+/*
+    We set up an event listener to respond to any new emails received by Thunderbird.
+ */
 messenger.messages.onNewMailReceived.addListener(async (folder, messages) => {
     console.log("New mail received")
 
+    /*
+        We use the messageTools.iterateMessagePages function to iterate over all the messages in the folder.
+     */
     for await (let message of messageTools.iterateMessagePages(messages)) {
         const full = await messenger.messages.getFull(message.id);
 
-        // Only process emails received in the last 24 hours
+        /*
+            If for some reason the email was received over a day ago, ignore it
+         */
         if (message.date < new Date() - 1000 * 60 * 60 * 24) {
             console.log("Skipping email " + message.subject + " received more than 24 hours ago")
             continue
         }
 
+        /*
+            Get the text content of the email, stripping out HTML and CSS
+         */
         const content = getBody(full)
+
+        /*
+            Call Ollama to generate a summary of the email.
+         */
         const summary = await getSummary(content)
 
+        /*
+            If Ollama isn't running or there was another error, log it and exit.
+         */
         if (!summary) {
             console.error("Failed to generate summary for email " + message.subject)
             continue
         }
 
         console.log(summary)
+
+        /*
+            Send the summary to Slack.
+         */
         await fetch("https://hooks.slack.com/services/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",
             {
                 method: "POST",
@@ -32,10 +54,19 @@ messenger.messages.onNewMailReceived.addListener(async (folder, messages) => {
     }
 })
 
+/**
+ * Get the text content of the email, stripping out HTML, CSS
+ * @param message The email object
+ * @returns {string} The text content of the email
+ */
 function getBody(message) {
     let body = ""
     if (message.body) {
-        body += removeWhiteSpace(removeCSSCommentsFromString(removeCSSFromString(convertToPlain(message.body))))
+        let text = convertToPlain(message.body)
+        text = removeCSSFromString(text)
+        text = removeCSSCommentsFromString(text)
+        text = removeWhiteSpace(text)
+        body += text
     }
     if (message.parts) {
         message.parts.forEach(part => {
@@ -46,6 +77,11 @@ function getBody(message) {
     return body
 }
 
+/**
+ * Convert HTML to plain text
+ * @param html The input HTML
+ * @returns {string} The text content
+ */
 function convertToPlain(html){
 
     // Create a new div element
@@ -58,6 +94,11 @@ function convertToPlain(html){
     return tempDivElement.textContent || tempDivElement.innerText || "";
 }
 
+/**
+ * Strip out CSS
+ * @param inputString The input text
+ * @returns {string} The text content without CSS
+ */
 function removeCSSFromString(inputString) {
     // Regular expression to match CSS style declarations
     const cssRegex = /.*?{[^}]*}/g;
@@ -66,6 +107,11 @@ function removeCSSFromString(inputString) {
     return outputString;
 }
 
+/**
+ * Remove CSS comments from the string
+ * @param inputString The input string
+ * @returns {string} The text content without comments
+ */
 function removeCSSCommentsFromString(inputString) {
     // Regular expression to match CSS style declarations
     const cssRegex = /\/\*.*?\*\//g;
@@ -74,6 +120,11 @@ function removeCSSCommentsFromString(inputString) {
     return outputString;
 }
 
+/**
+ * Remove unnecessary white space from the string
+ * @param message The input string
+ * @returns {string} The string without unnecessary white space
+ */
 function removeWhiteSpace(message) {
     return message
         .split("\n")
@@ -82,6 +133,11 @@ function removeWhiteSpace(message) {
         .replaceAll("  ", " ")
 }
 
+/**
+ * Call Ollama to generate a summary of the email
+ * @param content The plain text context of the email
+ * @returns {Promise<any>} The email summary
+ */
 function getSummary(content) {
     // Need to set the OLLAMA_ORIGINS=moz-extension://* environment variable for Ollama
     return fetch("http://localhost:11434/api/generate",
@@ -102,9 +158,4 @@ function getSummary(content) {
         .then(response => response.json())
         .then(data => data.response)
         .catch(error => console.error('Error:', error))
-}
-
-function textToHtml(text) {
-    let html = text.replace(/\n/g, '<br>');
-    return html;
 }
