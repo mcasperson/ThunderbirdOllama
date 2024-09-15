689ad2485f942dbb0184eca74e54396f2b2d98a0
Matthew Casperson <matthewcasperson@gmail.com>
2024-09-16 06:40:12 +1000

    Remove assignment to innerHtml

--- background.js
+++ background.js
@@ -130,15 +130,7 @@ function getBody(message) {
  * @returns {string} The text content
  */
 function convertToPlain(html){
-
-    // Create a new div element
-    const tempDivElement = document.createElement("div");
-
-    // Set the HTML content with the given value
-    tempDivElement.innerHTML = html;
-
-    // Retrieve the text property of the element
-    return tempDivElement.textContent || tempDivElement.innerText || "";
+    return html.replace(/<[^>]*>/g, '');
 }
 
 /**
